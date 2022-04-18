cc5d05367508b939708eb7e54363a63df23069e8
Willy Laurent Brenkman <88841329+LaurentBrenkman@users.noreply.github.com>
2022-04-19 00:37:59 +0700

    Tugas Hari ke 3

--- contact.js
+++ contact.js
@@ -1,36 +1,49 @@
 function showData() {
-    // DOM = Document Object Model
-    let name = document.getElementById("name").value
-    let email = document.getElementById("email").value
-    let phone = document.getElementById("phone").value
-    let subject = document.getElementById("subject").value
-    let message = document.getElementById("message").value
+    // DOM Obejct
+    let name= document.getElementById("name").value
+    let email= document.getElementById("email").value
+    let phone= document.getElementById("phone").value
+    let subject= document.getElementById("subject").value
+    let message= document.getElementById("message").value
+    
+    // Clear Data
+    document.getElementById ("name").value =""
+    document.getElementById ("email").value =""
+    document.getElementById ("phone").value =""
+    document.getElementById ("subject").value =""
+    document.getElementById ("message").value =""
 
-    document.getElementById("name").value = ""
-
-    // VALIDATION
+    // Validation
     if (name == "") {
-        return alert("Name input must not be empty")
+        return alert("Name input must not empty")
+    }
+    if (email == "") {
+        return alert("Email input must not empty")
+    }
+    if (phone == "") {
+        return alert("Phone input must not empty")
+    }
+    if (subject == "") {
+        return alert("Subject input must not empty")
+    }
+    if (message == "") {
+        return alert("Message input must not empty")
     }
 
-    let emailReceiver = "jokowi@gmail.com"
+    let emailReceiver = "rhomairama.dev@gmail.com"
 
     let a = document.createElement('a')
-    a.href = `mailto:${emailReceiver}?subject=${subject}&body=Hello, my name ${name}, ${subject}, ${message}, this is my phone ${phone}`
-    // a.href = "https://www.autofun.co.id/mobil"
+    a.href = `mailto:${emailReceiver}?subject=${subject}&body=Hello, my name ${name}, ${subject}, ${message}`
     a.click()
-
-    // kata-kunci nama = {
-    //     proprty-name : value
-    // }
 
     let dataObject = {
         name: name,
         email: email,
-        phone,
-        subject,
-        message,
+        phone: phone,
+        subject: subject,
+        message: message
     }
 
     console.table(dataObject)
+
 }
